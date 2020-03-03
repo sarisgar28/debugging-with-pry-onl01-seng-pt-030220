b28b3c041b743a0df23160d22f1c8f5a3c400878
Sara Isabel Garcia Reyes <sarisgar28@gmail.com>
2020-03-03 16:05:31 +0000

    Automatically backed up by Learn

--- lib/pry_is_awesome.rb
+++ lib/pry_is_awesome.rb
@@ -2,11 +2,14 @@ require 'pry'
 
 def prying_into_the_method
 	inside_the_method = "We're inside the method"
+	binding.pry
 	puts inside_the_method
+	binding.pry
 	puts "We're about to stop because of pry!"
 	binding.pry
 	this_variable_hasnt_been_interpreted_yet = "The program froze before it could read me!" 
 	puts this_variable_hasnt_been_interpreted_yet
+	binding.pry
 end
 
 prying_into_the_method
