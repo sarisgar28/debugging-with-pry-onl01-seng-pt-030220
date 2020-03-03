cb1646b9331994ebff54b308fdc110636857530a
Sara Isabel Garcia Reyes <sarisgar28@gmail.com>
2020-03-03 16:06:10 +0000

    Automatically backed up by Learn

--- lib/pry_is_awesome.rb
+++ lib/pry_is_awesome.rb
@@ -6,8 +6,8 @@ def prying_into_the_method
 	puts inside_the_method
 	binding.pry
 	puts "We're about to stop because of pry!"
-	binding.pry
 	this_variable_hasnt_been_interpreted_yet = "The program froze before it could read me!" 
+	binding.pry
 	puts this_variable_hasnt_been_interpreted_yet
 	binding.pry
 end
